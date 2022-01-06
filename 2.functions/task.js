130990c26593f03b805d3380f7c75e006ac57dee
MikhailPonomarev <michailp800@gmail.com>
2022-01-06 14:24:58 +0300

    Resolved first task in 2.functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,45 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+    let min, max, sum, avg;
 
-  // Ваш код
+    // Ваш код
+    min = arr[0];
+    max = arr[0];
+    sum = 0;
 
-  return { min: min, max: max, avg: avg };
+    for (let element of arr) {
+        if (element > max) {
+            max = element;
+        } else if (element < min) {
+            min = element;
+        }
+        sum += element;
+    }
+
+    avg = Number((sum / arr.length).toFixed(2));
+
+    return {min: min, max: max, avg: avg};
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+    let sum;
 
-  // Ваш код
+    // Ваш код
 
-  return sum;
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+    let max;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+    // Ваш кода
+    // for ...
+
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    // Ваш код
 }
